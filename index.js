e31b26bf707220a9f7cd2a9d09d61b950654cb02
Bhimashankar Sutar <bhima@piktorlabs.com>
2020-07-03 17:14:08 +0530

    added server code for sending rendered string.

---
+++ index.js
@@ -0,0 +1,46 @@
+import * as functions from "firebase-functions";
+import React from "react";
+import { renderToString } from "react-dom/server";
+import express from "express";
+
+import App from "./src/App";
+import getNews from "./src/getNews";
+
+const app = express();
+
+const getHtml = function (html) {
+  return `<!DOCTYPE html>
+ <html lang="en">
+   <head>
+     <meta charset="utf-8" />
+     <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+     <meta name="viewport" content="width=device-width, initial-scale=1" />
+     <meta name="theme-color" content="#000000" />
+     <meta
+       name="description"
+       content="Web site created using create-react-app"
+     />
+     <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+     
+     <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+     <title>Hacker News</title>
+   </head>
+   <body>
+     <noscript>You need to enable JavaScript to run this app.</noscript>
+     <div id="root">${html}</div>
+     <script type="text/javaScript" src="bundle.js"></script>
+   </body>
+ </html>
+ `;
+};
+
+app.get("**", (req, res) => {
+  getNews(0).then((news) => {
+    const html = renderToString(<App news={news.hits} />);
+    const finalHtml = getHtml(html);
+    res.send(finalHtml);
+  });
+});
+
+//cloud function.
+export let ssrApp = functions.https.onRequest(app);
